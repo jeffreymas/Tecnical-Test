5378fbf5ab9c9bfb3d806319fac598f11087d243
Jepri <55873259+jeffreymas@users.noreply.github.com>
2022-06-18 11:30:01 +0700

    test

---
+++ WebApp/ViewModel/ChartVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.ViewModel
+{
+    public class ChartVM
+    {
+    }
+    public class PieChartVM
+    {
+        public string DepartmentName { get; set; }
+        public int total { get; set; }
+    }
+
+}
